e225cf38d77768cbd996df3b3e24b0667b042988
ceigon01 <jsmith@ceigon.com>
2023-07-28 22:26:48 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav.component';
+import { MainContentComponent } from './main-content.component';
+import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    MainContentComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
